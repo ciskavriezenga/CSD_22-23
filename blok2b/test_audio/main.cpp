df6c0b3144e33ddaf3bb86d8685b56c8aa7c0fab
Ciska V <ciskavriezenga@gmail.com>
2022-11-25 19:47:02 +0100

    added a directory with a test project to test jack

---
+++ blok2b/test_audio/main.cpp
@@ -0,0 +1,43 @@
+
+#include "jack_module.h"
+#include <cmath>
+#include <iostream>
+
+
+class CustomCallback : public AudioCallback {
+public:
+    void prepare (int rate) override {
+        sampleRate = (float) rate;
+    }
+
+    void process (AudioBuffer buffer) override {
+        for (int i = 0; i < buffer.numFrames; ++i) {
+            phase += frequency / sampleRate;
+            // write sample to buffer at channel 0
+            buffer.outputChannels[0][i] = sin (phase * pi * 2.0f);
+        }
+    }
+
+private:
+    const float pi = acos (-1);  //atan(1) * 4; <-- vak van Pieter.
+    float phase = 0;
+    float frequency = 440;
+    float sampleRate = 48000;
+};
+
+// ================================================================================
+
+int main() {
+    auto callback = CustomCallback {};
+    auto jackModule = JackModule { callback };
+
+    jackModule.init (0, 1);
+
+    bool running = true;
+    while (running) {
+        switch (std::cin.get()) {
+            case 'q':
+                running = false;
+        }
+    }
+}
