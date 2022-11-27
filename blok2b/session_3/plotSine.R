61f74cacbe7a9d555ac85630d6d05d766ab5ab48
Ciska V <ciskavriezenga@gmail.com>
2022-11-27 10:20:24 +0100

    Went through the step by step explenation of how to generate a sine based on phase and t as sample index

--- blok2b/session_3/plotSine.R
+++ blok2b/session_3/plotSine.R
@@ -19,7 +19,8 @@ abline(h = 0, lty = 1, col = "gray")
 # samplerate = the number of samples per seconds
 # thus a sinewave with frequency = 1 corresponds to a wavelength = samplerate 
 samplerate = 44100
-# instead of finishing the wave in 1 t interval - we need to 'slow it down' to 44100 
+# instead of finishing the wave in 1 t interval (simlation 1 second),
+# we need to 'slow it down' to 44100 
 # thus 'divide' every step in 44100 smaller steps:
 sinewave <- function(t) sin(t * 2*pi / samplerate)
 curve(sinewave, 0, samplerate, xname = "x")
@@ -27,8 +28,10 @@ abline(h = 0, lty = 1, col = "gray")
 
 
 # now let's add the frequency, 
-# a sinewave with frequency = 2 has a two time shorter wavelength compared to a sinewave with frequency = 1
-# thus at every sample step it 'goes two times faster', we can simply multiply t
+# a sinewave with frequency = 2 has a two time shorter wavelength compared to 
+# a sinewave with frequency = 1
+# thus at every sample step it 'goes two times faster', 
+# therefore, we can simply multiply t with the frequency
 frequency <- 2
 sinewave <- function(t) sin(2* t * pi * frequency / samplerate)
 curve(sinewave, add = TRUE, col = "violet")
@@ -40,22 +43,24 @@ curve(sinewave, add = TRUE, col = "green")
 
 
 # ________________ usage of phase _________________
-
 # Let t indicate the index of a sample,
 # then the equation is as follow.
 # sample =  sin((t * 2 * pi) / samplerate)
-#
+
 # we can rewrite this function: 
 # sample =  sin((t / samplerate) * 2 * pi)
 #
-# (t / samplerate) with interval t = [0, samplerate]results in the interval [0, 1]
+# This results in a normalized value when t is in the range of [0, samplerate] 
+# (t / samplerate) with interval t = [0, samplerate] 
+# results in the interval [0, 1]
 # e.g. samplerate = 44100
 # 0 / 44100 = 0
 # 44100 / 44100 = 1
 
 # Because the sin function is a periodic function 
-# Therefore, with t > samplerate in (t/samplerate) * 2 * pi, the sine curve simply repeats
-# thus (t / samplerate) can be viewed as the phase of the sinewave
+# Therefore, with t > samplerate in (t/samplerate) * 2 * pi, 
+# the sine curve simply repeats.
+# Thus (t / samplerate) can be viewed as the phase of the sinewave
 # phase = t / samplerate
 
 # Let's rewrite the equation.
@@ -63,23 +68,35 @@ curve(sinewave, add = TRUE, col = "green")
 # with phase = t / samplerate 
 # results in: 
 # sample = sin(phase * 2 * pi)
-#
-# To generate a sinewave with frequency = 1, it is sufficient to keep track of the phase
+
+# To generate a sinewave with frequency = 1, 
+# it is sufficient to keep track of the phase
 # and to increase this at each sample step.
-# phase += frequenc / samplerate
-#
+# phase += 1 / samplerate
+
 # Now, let's also consider the frequency 
-# To also take the frequency into account, we can simply add it as follow.
+# To take the frequency into account, we can simply add it as follow.
 # phase += frequency / samplerate
 
-samplerate = 44100
-phase = 0
-frequency = 2
-sinewave <- function(t) {
-  phase = phase + (frequency / samplerate)
-  return (sin((t * frequency / samplerate) * 2*pi))
+{
+  samplerate = 44100
+  phase = 0
+  frequency = 1
+  phase_step = (frequency / samplerate)
+  sinewave <- function(sample_index) {
+    # phase = phase + phase_step
+    phase = sample_index * phase_step
+    return (sin(phase * 2*pi))
+  }
+  output = list()
+  for (i in 1:samplerate) {
+    output[i] = sinewave(i)
+  }
+  peakSampleI = 0.25 * samplerate
+  output[peakSampleI]
+  plot(c(1:samplerate), output)
 }
-curve(sinewave, 0, samplerate, xname = "x")
 
-
+# QUESTION: what are the benefits of working with a phase instead of time index? 
+# TIP: what happens in both scenarios when you alter the frequency?
 
